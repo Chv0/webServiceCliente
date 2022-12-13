b348d7372ddc92dad0dd2f488fcc9b7d0154db23
Chv0 <92190015+Chv0@users.noreply.github.com>
2022-12-13 17:47:18 -0500

    Add files via upload

---
+++ C#/ClienteWS/ClienteWS/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClienteWS
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceClienteWS.wsoperacionesClient clientes = new ServiceClienteWS.wsoperacionesClient();
+
+
+            //Loggin
+            if (clientes.Loggin("paver", "ta123"))
+            {
+                Console.WriteLine("Credenciales correctas");
+            }
+            else
+            {
+                Console.WriteLine("Credenciales incorrectas");
+            }
+
+            //realizar pago
+            if (clientes.ProcesarPago(50, 100)>=0)
+            {
+                Console.WriteLine("Pago realizado");
+            }
+            else
+            {
+                Console.WriteLine("Dinero  insuficiente");
+            }
+
+
+            clientes.Close();
+            Console.ReadKey();
+        }
+    }
+}
